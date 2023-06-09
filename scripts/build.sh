5dbbba66d5f802d6d4cc99d4b65511d4260362d1
lizhensheng <lizhensheng@actionsky.com>
2023-06-09 14:04:36 +0800

    [fix]: update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,3 +14,12 @@ mv ./todo-frontend.tar.gz ../../todo-frontend.tar.gz
 if [ -n "$(docker images -q -f dangling=true)" ]; 
 then docker rmi $(docker images -q -f dangling=true); 
 fi
+
+
+# remove unused volume
+volumes=$(docker volume ls -qf "dangling=true")
+
+if [ ! -z "$volumes" ]; then
+    echo "Removing volumes: $volumes"
+    echo "$volumes" | xargs docker volume rm
+fi
